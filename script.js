1675f20df62698f93c9edaac23c72890b8e07a27
Rishabh19280 <your-email@example.com>
2026-06-30 12:57:51 +0530

    Hero Bar Complete

--- script.js
+++ script.js
@@ -10,3 +10,92 @@ toggle.addEventListener("click", () =>{
 closeBtn.addEventListener("click", () =>{
     navMenu.classList.remove("active");
 })
+
+// const words = [
+//     "Fresh Food",
+//     "Best Taste",
+//     "Fast Delivery"
+// ];
+
+// let wordIndex = 0;
+// let charIndex = 0;
+// let isDeleting = false;
+
+// let text = document.getElementById("text");
+
+// function typeEffect() {
+
+//     let currentWord = words[wordIndex];
+
+//     if (!isDeleting) {
+//         text.textContent =
+//             currentWord.substring(0, charIndex + 1);
+
+//         charIndex++;
+
+//         if (charIndex === currentWord.length) {
+//             isDeleting = true;
+//             setTimeout(typeEffect, 1500);
+//             return;
+//         }
+
+//     } else {
+
+//         text.textContent =
+//             currentWord.substring(0, charIndex - 1);
+
+//         charIndex--;
+
+//         if (charIndex === 0) {
+//             isDeleting = false;
+//             wordIndex = (wordIndex + 1) % words.length;
+//         }
+//     }
+
+//     setTimeout(typeEffect, isDeleting ? 80 : 120);
+// }
+
+// typeEffect();
+
+
+let words = ["Fresh Food", "Best Teste", "Fast Delivery"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let text = document.getElementById("text");
+
+function typeEffect()
+{
+    let currentWord = words[wordIndex];
+
+    if(!isDeleting)
+    {
+        text.textContent = currentWord.substring(0, charIndex+1)
+
+        charIndex++;
+
+        if(charIndex === currentWord.length)
+        {
+            isDeleting = true;
+            setTimeout(typeEffect,1500);
+            return;
+        }
+    }
+    else
+    {
+        text.textContent = currentWord.substring(0, charIndex - 1);
+
+        charIndex--;
+        if(charIndex === 0)
+        {
+            isDeleting = false;
+
+            wordIndex = (wordIndex + 1) % words.length;           
+        }
+        
+    }
+    setTimeout(typeEffect, isDeleting ? 80 : 120);
+}
+
+typeEffect()
+
